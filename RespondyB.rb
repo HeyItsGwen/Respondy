c44276e359d91ee48c5cfe86ef45aa9bf830cda4
Gwen <itsgwenhey@gmail.com>
2020-01-07 16:47:15 -0600

    fix stickler errors for linelength

--- RespondyB.rb
+++ RespondyB.rb
@@ -3,9 +3,9 @@
 
 require 'rubygems'
 require 'chatterbot/dsl'
-# rubocop: disable Metrics/LineLength
+
 random_facts = ["Ruby, the programming language, was developed in the mid-1990's by Yukihiro 'Matz' Matsumoto in Japan! #Ruby #Facts #Programming", 'Ruby, the programming language, was influenced by Perl, Smalltalk, Eiffel, Ada, Basic, and Lisp! #Ruby #Facts #Programming', 'Ruby, the programming language, is dynamically typed and uses garbage collection! #Ruby #Facts #Programming', 'Ruby, the programming language, supports multiple programming paradigms, including procedural, object-oriented, and functional programming! #Ruby #Facts #Programming', 'Ruby, the programming language, was named in a chatroom before any code was ever written! #Ruby #Programming #Facts', 'Javascript is used so widely that all major web browsers have a dedicated engine to make it run! #Javascript', 'Javascript is a multi-paradigm language, supporting event-driven, functional, and imperative programming styles!', 'Java and Javascript have nothing to do with each other! #java #Javascript #JS', "HTML was developed in 1993... It's 27 years old!! #HTML #HTMLFacts", "CSS was invented in 1996, 3 years after HTML! IT's 23 years old! #CSSFacts #CSS"]
-# rubocop: enable Metrics/LineLength
+
 hashtag_array = ['#respondyb', '#Respondyb', '#respondyB', '#RespondyB']
 
 start_time = DateTime.new(2019, 12, 29, 0, 0, 0)
