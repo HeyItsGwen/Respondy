1b32e275f0204f04b0cdad41097203b44111f4ce
Gwen <itsgwenhey@gmail.com>
2020-01-07 22:52:28 -0600

    working redo
    Made a working redo from an older version of the program, and made the retweet function work by checking what the text of the tweet includes

--- RespondyB.rb
+++ RespondyB.rb
@@ -1,33 +1,46 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'rubygems'
 require 'chatterbot/dsl'
 
-random_facts = ["Ruby, the programming language, was developed in the mid-1990's by Yukihiro 'Matz' Matsumoto in Japan! #Ruby #Facts", "Ruby, the programming language, was influenced by Perl, Smalltalk, Eiffel, Ada, Basic, and Lisp! #Ruby #Facts", "Ruby, the programming language, is dynamically typed and uses garbage collection! #Ruby #Facts", "Ruby, the programming language, supports multiple programming paradigms, including procedural, object-oriented, and functional programming! #Ruby #Facts", "Ruby, the programming language, was named in a chatroom before any code was ever written!", "Javascript is used so widely that all major web browsers have a dedicated engine to make it run!", "Javascript is a multi-paradigm language, supporting event-driven, functional, and imperative programming styles!", "Java and Javascript have nothing to do with each other!", "HTML was developed in 1993... It's 27 years old!!", "CSS was invented in 1996, 3 years after HTML! IT's 23 years old!"]
+verbose
 
-hashtag_array = ["#respondyb","#Respondyb","#respondyB","#RespondyB"]
+blocklist 'RespondyB'
 
-start_time = DateTime.new(2019,12,29,0,0,0)
+@random_facts = ["Ruby, the programming language, was developed in the mid-1990's by Yukihiro 'Matz' Matsumoto in Japan! #Ruby #Facts", 'Ruby, the programming language, was influenced by Perl, Smalltalk, Eiffel, Ada, Basic, and Lisp! #Ruby #Facts', 'Ruby, the programming language, is dynamically typed and uses garbage collection! #Ruby #Facts', 'Ruby, the programming language, supports multiple programming paradigms, including procedural, object-oriented, and functional programming! #Ruby #Facts', 'Ruby, the programming language, was named in a chatroom before any code was ever written!', 'Javascript is used so widely that all major web browsers have a dedicated engine to make it run!', 'Javascript is a multi-paradigm language, supporting event-driven, functional, and imperative programming styles!', 'Java and Javascript have nothing to do with each other!', "HTML was developed in 1993... It's 27 years old!!", "CSS was invented in 1996, 3 years after HTML! IT's 23 years old!"]
+
+@start_time = DateTime.new(2019, 12, 29, 0, 0, 0)
+
+File.foreach('retweeted.txt') do |line|
+  @retweeted_array << line
+end
+
+def replyer
+  replies do |tweet|
+    reply "#USER# Did you know that #{@random_facts[rand(@random_facts.length)]}", tweet
+  end
+end
+
+def tweeter
+  if DateTime.now - @start_time >= 0.5
+    tweet("Did you know that #{@random_facts[rand(@random_facts.length)]}")
+    @start_time = DateTime.now
+  end
+end
+
+def hashtag_responder
+  search '#respondyb' do |tweet|
+    retweet(tweet.id) unless tweet.text.include?('RT @')
+  end
+end
 
 loop do
-  
-  replies do |tweet|
-    reply "#USER# Did you know that #{random_facts[rand(random_facts.length)]}", tweet
-  end
+  replyer
 
-  if DateTime.now() - start_time >= 0.5
-    tweet("Did you know that #{random_facts[rand(random_facts.length)]}")
-    start_time = DateTime.now()
-  end
+  tweeter
 
-  hashtag_array.each do |tag|
-    search "#{tag}" do |tweet|
-      retweet(tweet.id)
-    end
-  end
+  hashtag_responder
 
-  sleep 60
-
-end 
-
+  sleep 30
 end
